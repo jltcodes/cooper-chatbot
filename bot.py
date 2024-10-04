42de0b27ea4052c7a55321fe1e15d28423f2d7f3
jltcodes <jtengco5@gamil.com>
2024-10-04 20:33:16 +0800

    Refactor bot.py to change Streamlit layout to "wide"

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ from langchain.prompts import PromptTemplate
 import pandas as pd
 import threading
 
-st.set_page_config(page_title="Cooper", layout="centered")
+st.set_page_config(page_title="Cooper", layout="wide")
 
 # Google API Key
 api_key = st.secrets["default"]["GOOGLE_API_KEY"]
